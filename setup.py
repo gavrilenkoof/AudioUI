aed6c28388389577afda2637a0620614c82851b2
Ivan Haurylenka <hip@uvr.aero>
2023-03-20 16:38:19 +0300

    add setup for windows

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@ import os
 import sys
 from setuptools import setup
 
+version="1.1.0"
+upgrade_code = '{F46BA620-C027-4E68-9069-5D5D4E1FF30A}',
 
 HUMAN_FRIENDLY_NAME = 'AudioUI'
+HUMAN_FRIENDLY_NAME_DIR = HUMAN_FRIENDLY_NAME + f"_v{version}"
 
 company_name = 'UAV'
 product_name = HUMAN_FRIENDLY_NAME
 
-version="1.1.0"
-upgrade_code = '{F46BA620-C027-4E68-9069-5D5D4E1FF30A}',
-
 include_files = ["icons"]
+
+
 
 args = dict(
     name=HUMAN_FRIENDLY_NAME,
@@ -25,11 +27,12 @@ if ('bdist_msi' in sys.argv) or ('build_exe' in sys.argv):
     import cx_Freeze
 
     bdist_msi_options = {
-    'upgrade_code': '{F46BA620-C027-4E68-9069-5D5D4E1FF30A}',
-    'add_to_path': False,
-    'initial_target_dir': r'[ProgramFilesFolder]\%s\%s' % (company_name, product_name),
+        'upgrade_code': '{F46BA620-C027-4E68-9069-5D5D4E1FF30A}',
+        'add_to_path': False,
+        'initial_target_dir': r'[ProgramFilesFolder]\%s\%s' % (company_name, product_name),
     }
     build_exe_options = {
+        'build_exe': HUMAN_FRIENDLY_NAME_DIR,
         'include_files': include_files,
         'excludes': ['*'],
         "packages": [], 
@@ -44,7 +47,9 @@ if ('bdist_msi' in sys.argv) or ('build_exe' in sys.argv):
         cx_Freeze.Executable(script="main.py",
                              base="Win32GUI",
                              icon='icons/mic_icon.ico',
-                             shortcutName=HUMAN_FRIENDLY_NAME),
+                             shortcut_name=HUMAN_FRIENDLY_NAME,
+                             target_name=HUMAN_FRIENDLY_NAME,
+                             ),
     ]
 
-    cx_Freeze.setup(args)
+    cx_Freeze.setup(**args)
